f177fd08af776759d3994bb891f2231d21c0262e
SiddiqIbrahim <siddiqibrahim55555@gamil.com>
2025-12-26 18:46:24 +0530

    vite change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server:{
-host:true
+host:true,
+preview:{
+  allowedHosts:["https://x-clone-frontend-5bpx.onrender.com"]
+}
   },
   plugins: [
     tailwindcss(),    
